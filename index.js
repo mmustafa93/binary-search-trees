945b0ef880670ea9e7c416848941dcd0d54c9ba0
Muhammad Mustafa <75218580+mmustafa93@users.noreply.github.com>
2025-02-10 16:55:18 +0500

    Create Node and Tree classes. The Node class stores data for the binary search tree and the Tree class generates a binary tree based on an input array

---
+++ index.js
@@ -0,0 +1,38 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree {
+    constructor(array){
+        // Remove duplicates and sort the array before building the tree
+        const sortedArray = [...new Set(array)].sort((a, b) => a - b);
+        this.root = this.buildTree(sortedArray);
+        } 
+
+    buildTreeRecur(array, start, end) {
+        if (start > end) return null;
+
+        // Find the middle element
+        let mid = start + Math.floor((end - start) / 2);
+
+        // Create root node
+        let root = new Node(array[mid]);
+
+        // Create left subtree
+        root.left = this.buildTreeRecur(array, start, mid - 1);
+
+        // Create right subtree
+        root.right = this.buildTreeRecur(array, mid + 1, end);
+
+        return root;
+    }
+
+    buildTree(array) {
+        return this.buildTreeRecur(array, 0, array.length - 1)
+    }
+}
+
